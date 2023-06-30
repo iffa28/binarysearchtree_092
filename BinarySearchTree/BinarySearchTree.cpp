0041ec5d8f674e46c44872ea248e2118ed1b0a9b
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-06-30 07:30:30 +0700

    fungsi untuk mencari Node saat ini dari Node yang ditentukan serta Node saat ini dari induknya

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -57,8 +57,8 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)   
 	{
-		//this function searches the currentNode of th e specified Node as well as the current Node of its parent
-		currentNode = ROOT;
+		//this function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT; 
 		parent = NULL;
 		while ((currentNode != NULL) && (currentNode->info != element))
 		{
