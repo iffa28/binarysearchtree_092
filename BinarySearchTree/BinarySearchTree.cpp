de68e6f3e28838d2444b7e367ddb1bda5a83c002
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-06-30 07:31:16 +0700

    current node sama dengan root

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -62,7 +62,7 @@ public:
 		parent = NULL;
 		while ((currentNode != NULL) && (currentNode->info != element))
 		{
-			parent = currentNode;
+			parent = currentNode; 
 			if (element < currentNode->info)
 				currentNode = currentNode->leftchild;
 			else
